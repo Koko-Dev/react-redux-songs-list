cde2f095606976bfdc59cd92f07a3dc3b867ba95
Koko-Dev <koko.webdev@gmail.com>
2022-04-22 16:01:31 -0400

    In src/index.js, because we want to understand the basics of Redux, we will continue to use createStore and not switch to RTK just yet; import the legacy createStore instead of configureStore from redux; surround <App /> with component <Provider store={createStore}> <App /> </Provider>

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { configureStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
+/*
+	NOTE:
+	 // Redux STORE
+	 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+	 const prodMiddleware = [thunk, immutableStateInvariant, serializableStateInvariant]:
+	 const devMiddleware = [thunk];
+	 const middleware = process.env.prod ? prodMiddleware : devMiddleware;
+	 const enhancer = composeEnhancers(applyMiddleware(...middleware));
+
+	 NOTE: const ReduxStore = createStore(rootReducer, enhancer);
+
+	 NOTE: // RTK STORE
+	  const RTKStore = configureStore({reducer: rootReducer}); */
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+	  <Provider store={createStore(reducers)}>
+		  <App />
+	  </Provider>
   </React.StrictMode>
 );
 
