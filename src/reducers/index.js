d39ce16cdc60252084322d64647f60b8262288ba
Koko-Dev <koko.webdev@gmail.com>
2022-04-22 14:33:28 -0400

    In reducers/index.js, we have to wire up the reducers using combineReducers from redux; export combineReducers whose argument is an object which contains songs: songsReducer and selectedSong: selectedSongReducer;  this should be the end of the Redux side of things
    // src/reducers/index.js
    
    import {combineReducers} from "redux";
    
    const songsReducer = () => {
        return [
            {title: 'No Scrubs', duration: '4:05'},
            {title: 'Macarena', duration: '2:30'},
            {title: 'All Star', duration: '3:15'},
            {title: 'I want it That Way', duration: '1:45'}
        ]
    }
    
    const selectedSongReducer = (selectedSong = null, action) => {
        if (action.type === 'SONG_SELECTED') {
            return action.payload;
        }
        return selectedSong;
    }
    
    export default combineReducers({
        songs: songsReducer,
        selectedSong: selectedSongReducer
    })
    
    -------------

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -14,6 +14,10 @@ const selectedSongReducer = (selectedSong = null, action) => {
 	if (action.type === 'SONG_SELECTED') {
 		return action.payload;
 	}
-
 	return selectedSong;
 }
+
+export default combineReducers({
+	songs: songsReducer,
+	selectedSong: selectedSongReducer
+})
