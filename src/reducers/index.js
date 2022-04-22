db520fe3f053a92666904f0f9a6654fe07fb46d9
Koko-Dev <koko.webdev@gmail.com>
2022-04-22 14:11:04 -0400

    Refactor src/reducers/index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,17 @@
 // Returns a static list of songs - a static array
 const songsReducer = () => {
 	return [
-		{ title: 'No Scrubs', duration: '4:05'},
-		{ title: 'Macarena', duration: '2:30'},
-		{ title: 'All Star', duration: '3:15'},
-		{ title: 'I want it That Way', duration: '1:45'}
+		{title: 'No Scrubs', duration: '4:05'},
+		{title: 'Macarena', duration: '2:30'},
+		{title: 'All Star', duration: '3:15'},
+		{title: 'I want it That Way', duration: '1:45'}
 	]
 }
 
-const selectedSongReducer = (selectedSong=null, action) => {
+const selectedSongReducer = (selectedSong = null, action) => {
 	if (action.type === 'SONG_SELECTED') {
 		return action.payload;
 	}
-
+	
 	return selectedSong;
 }
