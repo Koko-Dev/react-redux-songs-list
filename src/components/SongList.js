f16af24895a17639aac669ba4bbdab845fe9912a
Koko-Dev <koko.webdev@gmail.com>
2022-04-24 13:13:20 -0400

    SongList.js: In mapStateToProps(state), instead of returning state argument which includes list of songs and selected song, modify return statement to return an object with property songs whose value is state.songs (the list of songs)

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -10,12 +10,8 @@ class SongList extends Component {
 	}
 }
 
-// Note: state will be all of our data inside of redux store,
-//  i.e. list of songs from reducer/ songsReducer and
-//  selected song from reducer/ selectedSongReducer
 const mapStateToProps = (state) => {
-	console.log(state);
-	return state;
+	return { songs: state.songs };
 }
 
 export default connect(mapStateToProps)(SongList);
