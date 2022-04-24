b1e860011d33533d87c861e76cafc0f0801b6843
Koko-Dev <koko.webdev@gmail.com>
2022-04-24 13:01:44 -0400

    SongList.js: Create mapStateToProps(state) arrow function which receives all of the data inside of the redux store ( i.e. songs and selectedSong ) which were combined in reducers/index.js; this function will return the state of each property (songs and selectedSongs); modify export default to pass this function as an argument to => connect(mapStateToProps)(SongList)
    // SongList.js
    import React, { Component } from 'react';
    import { connect } from "react-redux";
    
    class SongList extends Component {
        render() {
            return (
                <div>SongList</div>
            )
        }
    }
    
    const mapStateToProps = (state) => {
        console.log(state);
        return state;
    }
    
    export default connect(mapStateToProps)(SongList);

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -10,5 +10,12 @@ class SongList extends Component {
 	}
 }
 
+// Note: state will be all of our data inside of redux store,
+//  i.e. list of songs from reducer/ songsReducer and
+//  selected song from reducer/ selectedSongReducer
+const mapStateToProps = (state) => {
+	console.log(state);
+	return state;
+}
 
-export default connect()(SongList);
+export default connect(mapStateToProps)(SongList);
