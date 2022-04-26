ff22a88afa63106d1232af9af459ca398c9a734f
Koko-Dev <koko.webdev@gmail.com>
2022-04-26 19:36:37 -0400

    SongDetail.js: Destructure SongDetail argument to extract only song; return a note to user that a song should be selected and display song title on Select; also, make the Component return a multiline return statement which displays the selected song's details, to include title and duration
    // SongDetail.js
    import React from 'react';
    import {connect} from 'react-redux';
    
    const SongDetail = ({ song }) => {
        if(!song) {
            return <div>Select a Song</div>;
        }
    
        return (
            <div>
                <h3>Details for: </h3>
                <p>
                    Title: {song.title} <br/>
                    Duration: {song.duration}
                </p>
            </div>
        );
    }
    
    const mapStateToProps = (state) => {
        return {song: state.selectedSong};
    }
    
    export default connect(
        mapStateToProps
    )(SongDetail);

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -3,10 +3,21 @@ import {connect} from 'react-redux';
 
 const SongDetail = ({ song }) => {
 	if(!song) {
-		return <div>Select a Song</div>
+		return <div>Select a Song</div>;
 	}
 
-	return <div>{song.title}</div>;
+	return (
+		<div>
+			<h3>Details for: </h3>
+			<p>
+				Title: {song.title} <br/>
+				Duration: {song.duration}
+			</p>
+
+
+
+		</div>
+	);
 }
 
 const mapStateToProps = (state) => {
